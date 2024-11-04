7c56f01e2e6248e4a4523b985d5628b1b03c6bd5
Your Name <shado@LAPTOP-U7GG9IKM>
2024-11-04 19:15:01 +0800

    initial commit

---
+++ colorPreferences.js
@@ -0,0 +1,13 @@
+function colorPreferences() {
+    favColors = [];
+    color1 = prompt("What is your first favorite color?");
+    favColors.push(color1);
+    alert(favColors);
+    color2 = prompt("What is your second favorite color?");
+    favColors.push(color2);
+    alert(favColors);
+    color3 = prompt("What is your third favorite color?");
+    favColors.push(color3);
+    console.log(favColors);
+    alert(favColors);
+}
